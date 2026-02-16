32abce13d9e95ca176307366f88e9c2b9d990dc4
Py Colors <patweb44@gmail.com>
2026-02-16 10:36:55 +0100

    chore(eslint): ignore generated Fumadocs files (.source) in marketing app
    Prevents linting of files within the .source directory
    to reduce unnecessary warnings or checks on generated or
    non-source files, streamlining the linting process.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,9 @@
-import { nextJsConfig } from '@pycolors/eslint-config/next-js';
+import { nextJsConfig } from "@pycolors/eslint-config/next-js";
 
 /** @type {import("eslint").Linter.Config[]} */
-export default nextJsConfig;
+export default [
+  ...nextJsConfig,
+  {
+    ignores: [".source/**"],
+  },
+];
