f27e9e020c598c217bf7861a1ebeaad4d95db3d3
P@trice <hello@pycolors.com>
2026-09-15 11:08:21 +0200

    style(blocks): apply prettier output to preview

--- components/marketing/blocks/block-catalog-preview.tsx
+++ components/marketing/blocks/block-catalog-preview.tsx
@@ -43,7 +43,9 @@ function RecoveryFormPreview() {
   );
 }
 
-export function BlockCatalogPreview({ blockId }: Readonly<{ blockId: string }>) {
+export function BlockCatalogPreview({
+  blockId,
+}: Readonly<{ blockId: string }>) {
   switch (blockId) {
     case "responsive-sidebar":
       return <ResponsiveSidebarExample />;
